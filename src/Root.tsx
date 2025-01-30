f093f52c3ea18095307a7a02c5c6ed1ba0b0d32e
YuriiMykhailenko <urijmihajlenko3@gmail.com>
2025-01-30 10:27:24 +0200

    fix import

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
-import { DetailsPage } from './pages/Details';
+
+import { DetailsPage } from './pages/DetailsPage';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { HomePage } from './pages/Home';
+import { HomePage } from './pages/HomePage';
 
 export const Root = () => {
   return (
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,12 +11,7 @@ export const HomePage = () => {
   }, []);
 
   return (
-    <section
-      className="content-cente grid w-full gap-x-12 
-      gap-y-16 justify-self-center px-24 py-32 text-bg 
-      tablet:grid-cols-2 tablet:px-32 tablet:py-48 
-      tablet-large:grid-cols-3 desktop:grid-cols-4"
-    >
+    <section className="content-cente grid w-full gap-x-12 gap-y-16 justify-self-center px-24 py-32 text-bg tablet:grid-cols-2 tablet:px-32 tablet:py-48 tablet-large:grid-cols-3 desktop:grid-cols-4">
       {!!citiesWeather &&
         citiesWeather.map(el => {
           return <Card city={el} key={el.id} />;
